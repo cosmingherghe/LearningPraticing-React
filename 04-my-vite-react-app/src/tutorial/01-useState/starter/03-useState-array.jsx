73becb3250f43875e4dac052f7c3f14c95ce94a1
cosmingherghe <cosminlinux@gmail.com>
2023-04-30 01:28:07 -0400

    UseState Array Example

--- 04-my-vite-react-app/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 04-my-vite-react-app/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,42 @@
+import { useState } from "react";
+import { data } from "../../../data";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  const removeAllItems = () => {
+    setPeople([]);
+  };
+
+  return (
+    <>
+      <h2>useState array example</h2>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div>
+            <div key={id}>
+              <div>{name}</div>
+              <button
+                type="button"
+                onClick={() => removeItem(id)}
+                className="btn"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        );
+      })}
+      <br />
+      <button type="button" onClick={removeAllItems} className="btn">
+        Remove All
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
